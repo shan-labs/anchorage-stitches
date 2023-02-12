ffcedda6449d207bf308690fa91353ba2a1f83a0
Khinshan Khan <shan@uptogood.dev>
2023-02-12 09:53:15 -0500

    add tailwind

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import React, { type ReactNode } from "react"
 
+import "../styles/globals.css"
+
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import React from "react"
 import type { NextPage } from "next"
 
 export const Page: NextPage = () => {
-  return <h1>Hello, Next.js!</h1>
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>
 }
 
 export default Page
