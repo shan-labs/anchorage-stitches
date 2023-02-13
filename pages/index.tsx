28a43033fe52c9b14511c48bc47ba102e92329c2
Khinshan Khan <shan@uptogood.dev>
2023-02-13 17:15:24 -0500

    poc to use old palette in tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import { ThemeToggle } from "components/theme-toggle"
 export default function Page() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-plum-8">Hello world!</h1>
       <ThemeToggle />
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,9 @@
+function hslaN(variableName, n, tokenName = undefined) {
+  return Array.from({ length: n }, (_, index) => index + 1).reduce((stored, num) => {
+    return { ...stored, [num]: `hsla(var(--${variableName}${num}), <alpha-value>)` }
+  }, {})
+}
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -7,7 +13,15 @@ module.exports = {
   ],
   darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: hslaN("gray", 12),
+        mauve: hslaN("mauve", 12),
+        plum: hslaN("plum", 12),
+        violet: hslaN("violet", 12),
+        bluegray: hslaN("bluegray", 12),
+      },
+    },
   },
   plugins: [],
 }
