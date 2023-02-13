2ec0a356c0df08b4f2f6d36a5c7b361e9d90a90a
Khinshan Khan <shan@uptogood.dev>
2023-02-13 15:50:42 -0500

    try injecting custom colors

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import { ThemeToggle } from "components/theme-toggle"
 
 export default function Page() {
   return (
-    <div className="bg-orange-200 dark:bg-blue-300 min-h-screen">
+    <div>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <ThemeToggle />
     </div>
