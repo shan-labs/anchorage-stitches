934b0ac93dabe9b25431993e183ae97d9590dc40
Khinshan Khan <shan@uptogood.dev>
2023-02-15 21:54:42 -0500

    allow localized variables

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,8 @@ export default function Page() {
   return (
     <div>
       <h1 className="text-plum-8">Hello world!</h1>
+
+      <h2 className="text-plum-2">Hello world!</h2>
       <ThemeToggle />
     </div>
   )
