d84b5288190d0818dd592c125b8e28512bbc9421
Khinshan Khan <shan@uptogood.dev>
2023-02-16 21:48:50 -0500

    use prettier to sort tailwind classes

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ export default function Page() {
     <div className="relative z-base">
       <h1 className="text-plum-8">Hello world!</h1>
 
-      <h2 className="">Hello world!</h2>
+      <h2 className="p-4 pt-2">Hello world!</h2>
       <ThemeToggle />
     </div>
   )
