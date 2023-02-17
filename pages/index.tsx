ed43efdad990e67163a68544aa128fd437849fcb
Khinshan Khan <shan@uptogood.dev>
2023-02-16 21:40:23 -0500

    experiment with object like notion for tailwind applies

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,10 +3,10 @@ import { ThemeToggle } from "components/theme-toggle"
 
 export default function Page() {
   return (
-    <div>
+    <div className="relative z-base">
       <h1 className="text-plum-8">Hello world!</h1>
 
-      <h2 className="text-plum-2">Hello world!</h2>
+      <h2 className="">Hello world!</h2>
       <ThemeToggle />
     </div>
   )
