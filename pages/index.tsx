066f7933126747d131893993d05a2cd4c16805b6
Khinshan Khan <shan@uptogood.dev>
2023-02-19 23:05:56 -0500

    first attempt at typography

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,9 +4,13 @@ import { ThemeToggle } from "components/theme-toggle"
 export default function Page() {
   return (
     <div className="relative z-base">
-      <h1 className="text-plum-8">Hello world!</h1>
-
-      <h2 className="p-4 pt-2">Hello world!</h2>
+      <h1>h1: Hello world!</h1>
+      <h2>h2: Hello world!</h2>
+      <h3>h3: Hello world!</h3>
+      <h4>h4: Hello world!</h4>
+      <h5>h5: Hello world!</h5>
+      <h6>h6: Hello world!</h6>
+      <p>p: hello</p>
       <ThemeToggle />
     </div>
   )
